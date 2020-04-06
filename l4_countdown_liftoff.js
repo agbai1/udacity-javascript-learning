5241d882766fc58a8287cf8643d7e687ec56393f
agbai1 <paulekeagbai@gmail.com>
2020-04-06 08:49:51 -0400

    Add code for nasa countdown, liftoff quiz

---
+++ l4_countdown_liftoff.js
@@ -0,0 +1,7 @@
+/*
+Directions:
+Write a while loop that counts down from 60 seconds and:
+
+If there's a task being completed, it prints out the task
+If there is no task being completed, it prints out the time as T-x seconds
+*/
