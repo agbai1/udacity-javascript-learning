9433fe705ce444c47339482c26f4acdcc5a1e433
agbai1 <paulekeagbai@gmail.com>
2020-04-10 23:25:07 -0400

    Modify the code to use map() to perform same operation

--- anotherTypeOfLoop_6-8.js
+++ anotherTypeOfLoop_6-8.js
@@ -1,12 +1,13 @@
 /*
 Directions:
-Use the array's forEach() method to loop over the following array and add 100 to each of the values if the value is divisible by 3.
-
-var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4, 19, 300, 3775, 299, 36, 209, 148, 169, 299, 6, 109, 20, 58, 139, 59, 3, 1, 139];
-Remember that the "Test Run" button will display any logged content, so feel free to use console.log() to test your code.
+Use the array's forEach() method to loop over 
+the following array and add 100 to each of the 
+values if the value is divisible by 3.
 */
 
-var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4, 19, 300, 3775, 299, 36, 209, 148, 169, 299, 6, 109, 20, 58, 139, 59, 3, 1, 139];
+var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4, 
+19, 300, 3775, 299, 36, 209, 148, 169, 299, 6, 109, 20, 
+58, 139, 59, 3, 1, 139];
 
 
 test.forEach(function(element,index,array) {
@@ -17,3 +18,21 @@ test.forEach(function(element,index,array) {
 });
 
 console.log(test);
+
+
+
+// Using map to do the same thing
+
+var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4, 
+19, 300, 3775, 299, 36, 209, 148, 169, 299, 6, 109, 20, 
+58, 139, 59, 3, 1, 139];
+
+
+let newArr = test.map(function(element) {
+  if (element % 3 === 0) {
+    element += 100;
+  }
+  return element;
+});
+
+console.log(newArr);
